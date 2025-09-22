5e2329a9ab4ae5f5d7e9f73ba141caaa12c05c86
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-09-22 16:08:29 +0530

    feat: created going on trip section

--- src/types/home.ts
+++ src/types/home.ts
@@ -1,8 +1,10 @@
+type img = {
+  src: string;
+  alt: string;
+};
+
 export type heroSection = {
-  image: {
-    src: string;
-    alt: string;
-  };
+  image: img;
   youFocus: string;
   manageYourStay: string;
   description: {
@@ -11,6 +13,22 @@ export type heroSection = {
   }[];
 };
 
+export type goingOnTripSection = {
+  trip: string;
+  yourStay: string;
+  guides: {
+    id: number;
+    src: string;
+    alt: string;
+    title: string;
+    description: string;
+    width: number;
+    height: number;
+  }[];
+  borderImg: img;
+};
+
 export type homeData = {
   heroSection: heroSection;
+  goingOnTripSection: goingOnTripSection;
 };
