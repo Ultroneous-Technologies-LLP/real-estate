2735e27d61b45ab8b3b4bc110bab119842dccd4e
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-09-30 16:44:51 +0530

    fix: updated constants folder

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,25 +1,15 @@
-const BREAK_POINT_LG = 1024;
-const BREAK_POINT_MD = 768;
-const BREAK_POINT_Mobile = 767;
+export const BREAK_POINT_LG = 1024;
+export const BREAK_POINT_MD = 768;
+export const BREAK_POINT_Mobile = 767;
 
-const FACEBOOK_URL = "https://facebook.com";
-const TWITTER_URL = "https://twitter.com";
-const INSTAGRAM_URL = "https://instagram.com";
+export const FACEBOOK_URL = "https://facebook.com";
+export const TWITTER_URL = "https://twitter.com";
+export const INSTAGRAM_URL = "https://instagram.com";
 
-const LOCATION_OPTIONS: string[] = [
+export const LOCATION_OPTIONS: string[] = [
   "Los Angeles",
   "San Diego",
   "San Francisco",
   "Sacramento",
   "Oakland",
 ];
-
-export {
-  BREAK_POINT_LG,
-  BREAK_POINT_MD,
-  BREAK_POINT_Mobile,
-  LOCATION_OPTIONS,
-  FACEBOOK_URL,
-  TWITTER_URL,
-  INSTAGRAM_URL,
-};
