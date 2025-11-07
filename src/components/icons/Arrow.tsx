81581ec76768177bfaa94a7a4dd2d546cae76914
Vijaydev19 <dev19.ultroneous@gmail.com>
2025-11-07 10:17:08 +0530

    fix: refactor icon components to use constants for size and improve viewBox attributes

--- src/components/icons/Arrow.tsx
+++ src/components/icons/Arrow.tsx
@@ -1,13 +1,19 @@
-import { SVGProps } from "react";
+import { JSX, SVGProps } from "react";
 
-export const Arrow = ({ width = 15, height = 15, ...svgProps }: SVGProps<SVGSVGElement>) => (
+import { ICON_SIZE_0, ICON_SIZE_15 } from "./constant";
+
+export const Arrow = ({
+  width = ICON_SIZE_15,
+  height = ICON_SIZE_15,
+  ...svgProps
+}: SVGProps<SVGSVGElement>): JSX.Element => (
   <svg
-    viewBox="0 0 15 15"
     fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    width={width}
     height={height}
     role="img"
+    viewBox={`${ICON_SIZE_0} ${ICON_SIZE_0} ${ICON_SIZE_15} ${ICON_SIZE_15}`}
+    width={width}
+    xmlns="http://www.w3.org/2000/svg"
     {...svgProps}
   >
     <path
